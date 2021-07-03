850d4b6e3d8c5e39b6b96f77ff2c910ca9beb01c
3Gigs <yanghuaxuan@gmail.com>
2021-07-02 21:07:38 -0700

    Took way longer than expected, but fully working now

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,7 +1,47 @@
-export function MakeBoard(props) {
+import React from "react";
+
+class MakeTasks extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tasks: [], 
+        }
+        this.AddTask = this.AddTask.bind(this);
+    }
+    AddTask() {
+        const task = prompt("Enter task");
+        this.setState((prevState) => {
+            return {tasks: [...prevState.tasks, task]}
+        })
+    }
+    render() {
+        return(
+            <div className="TaskTable">
+                <button onClick={this.AddTask}>Add task</button>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Task</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.tasks.map(task => (
+                            <tr key={task}>
+                                <td>{task}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}   
+
+export function MakeBoard() {
     return (
-        <div class="ToDoList">
-            <h4>Tasks to complete</h4>      
+        <div className="ToDoList">
+            <h4>Tasks to complete</h4>
+            <MakeTasks />
         </div>
     )
 }
